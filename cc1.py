40147b71bb9d5ba02ec805d3dd970c4b9e7ee67b
hclymer <87190908+hclymer@users.noreply.github.com>
2024-03-07 19:32:12 -0500

    Add files via upload

---
+++ cc1.py
@@ -0,0 +1,21 @@
+# Turn this line into a comment
+
+cities = ["Seattle", "Tacoma", "Bellevue"]
+
+a = 1
+print(type(a))
+
+b = 2.5 
+print(type(b))
+
+c = "John"
+print(type(c))
+
+d = a + b
+print(d)
+
+
+
+
+
+
